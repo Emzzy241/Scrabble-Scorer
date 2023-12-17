f6b0b4a8a9e1abda646417e84cfc7cbb687275fe
Emzzy241 <emzzyoluwole@gmail.com>
2023-12-17 23:19:12 +0100

    add UI logic, time to push

--- ScrabbleScorer/Program.cs
+++ ScrabbleScorer/Program.cs
@@ -8,7 +8,56 @@ namespace ScrabbleScorer.Models
         // bn=
         static void Main()
         {
-            
+            Console.WriteLine("Scrabble Score Calculator");
+            Console.WriteLine();
+            Console.WriteLine();
+            try
+            {
+                Console.Write("Enter a word: ");
+                string inputWord = Console.ReadLine();
+
+                MyScrabbleScorer newScrabble = new MyScrabbleScorer(inputWord);
+
+                int scrabbleScore = newScrabble.ScrabbleScoreDeterminer();
+                Console.WriteLine();
+                Console.WriteLine();
+                
+                Console.WriteLine($"The Scrabble score for '{inputWord}' is: {scrabbleScore}");
+                
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("To play again. Enter 'Y', to exit enter 'N");
+                string userContinue = Console.ReadLine().ToUpper();
+
+                if (userContinue == "Y")
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Okay, lets play again");
+                    Console.WriteLine();
+                    Main();
+                }
+                else if (userContinue == "N")
+                {
+                    Console.WriteLine("Goodbye");
+                }
+                else
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("Sorry, I did not get that. Please try again");
+                    Console.WriteLine();
+                    Main();
+                }
+
+            }
+            catch 
+            {
+                Console.WriteLine("Ooops!!!");
+                Console.WriteLine("Enter only numbers please. Try again");
+                Console.WriteLine();
+                Console.WriteLine();
+                 Main();
+            }
+
         }        
     }
 }
